dd88dd51aff0ae7bf1bfee8e4840b3db8095a785
Sanensi <william.picard@hotmail.com>
2021-04-11 18:31:05 -0400

    Add Paths utilities

---
+++ src/client/hooks/useNavigation.ts
@@ -0,0 +1,29 @@
+import { pathsOf } from "../utils/Paths";
+
+const root = pathsOf({
+  PATH: '/',
+  local: {
+    PATH: 'local',
+    game: {
+      PATH: 'game'
+    }
+  },
+  online: {
+    PATH: 'online',
+    lobby: {
+      PATH: 'lobby',
+      room: {
+        PATH: 'room'
+      }
+    },
+    game: {
+      PATH: 'game'
+    }
+  }
+});
+
+function useNavigation() {
+  return {
+    root
+  }
+}
